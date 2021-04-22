0999c8423ef0da2d3b3bacc93ef9993942420a7a
whayter <hwinston@student.42.fr>
2021-04-22 14:50:32 +0200

    modif

--- philo_one/inc/philosophers.h
+++ philo_one/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 12:05:56 by hwinston          #+#    #+#             */
-/*   Updated: 2021/04/21 19:14:52 by hwinston         ###   ########.fr       */
+/*   Updated: 2021/04/22 14:46:13 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ typedef struct      s_env
     pthread_mutex_t *forks;
     int             run;
     pthread_mutex_t lock;
+    struct timeval	t_start;
 }                   t_env;
 
 extern t_env g_env;
--- philo_one/src/routine.c
+++ philo_one/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 15:30:56 by hwinston          #+#    #+#             */
-/*   Updated: 2021/04/21 16:04:38 by hwinston         ###   ########.fr       */
+/*   Updated: 2021/04/22 14:47:35 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void        philosopher_die()
     i = -1;
     while (++i < g_env.n_phi)
     {
-        strt = get_time_since(g_env.phi[i].t_start);
+        //strt = get_time_since(g_env.phi[i].t_start);
+        strt = get_time_since(g_env.t_start);
         if (!g_env.phi[i].rounds)
         {
             if (strt >= g_env.t_die)
@@ -41,30 +42,37 @@ static void philosopher_eat(t_philo *phi)
 {
     pthread_mutex_lock(&g_env.lock);
     pthread_mutex_lock(&g_env.forks[phi->left_fork]);
-    display_status(phi->id, get_time_since(phi->t_start), FRK);
+    //display_status(phi->id, get_time_since(phi->t_start), FRK);
+    display_status(phi->id, get_time_since(g_env.t_start), FRK);
+
     pthread_mutex_lock(&g_env.forks[phi->right_fork]);
-    display_status(phi->id, get_time_since(phi->t_start), FRK);
+    //display_status(phi->id, get_time_since(phi->t_start), FRK);
+    display_status(phi->id, get_time_since(g_env.t_start), FRK);
+    
+    pthread_mutex_unlock(&g_env.lock);
     if (g_env.run)
     {
-        display_status(phi->id, get_time_since(phi->t_start), EAT);
+        //display_status(phi->id, get_time_since(phi->t_start), EAT);
+        display_status(phi->id, get_time_since(g_env.t_start), EAT);
         usleep(g_env.t_eat * 1000);
         get_actual_time(&phi->t_last);
         phi->rounds++;
     }
     pthread_mutex_unlock(&g_env.forks[phi->left_fork]);
     pthread_mutex_unlock(&g_env.forks[phi->right_fork]);
-    pthread_mutex_unlock(&g_env.lock);
 }
 
 static void philosopher_sleep(t_philo *phi)
 {
-    display_status(phi->id, get_time_since(phi->t_start), SLP);
+    //display_status(phi->id, get_time_since(phi->t_start), SLP);
+    display_status(phi->id, get_time_since(g_env.t_start), SLP);
     usleep(g_env.t_slp * 1000);
 }
 
 static void philosopher_think(t_philo *phi)
 {
-    display_status(phi->id, get_time_since(phi->t_start), THK);
+    //display_status(phi->id, get_time_since(phi->t_start), THK);
+    display_status(phi->id, get_time_since(g_env.t_start), THK);
 }
 
 void        *routine(void *phi)
