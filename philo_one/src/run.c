223d92425b67dfb1268bdcf6534bad677627fdc6
whayter <hwinston@student.42.fr>
2021-04-24 11:04:28 +0200

    small fix

--- philo_one/src/run.c
+++ philo_one/src/run.c
@@ -6,7 +6,7 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 10:52:14 by hwinston          #+#    #+#             */
-/*   Updated: 2021/04/24 11:00:15 by hwinston         ###   ########.fr       */
+/*   Updated: 2021/04/24 11:04:12 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void start_simulation()
     int i;
 
     g_env.run = 1;
-    get_actual_time(&g_env.t_start);
     i = 0;
+    get_actual_time(&g_env.t_start);
     while (i < g_env.n_phi)
     {
-        g_env.phi[i].t_last = g_env.t_start;
+        get_actual_time(&g_env.phi[i].t_last);
         pthread_create(&g_env.phi[i].thread, NULL, routine, &g_env.phi[i]);
         i += 2;
     }
@@ -29,7 +29,7 @@ void start_simulation()
     i = 1;
     while (i < g_env.n_phi)
     {
-        g_env.phi[i].t_last = g_env.t_start;
+        get_actual_time(&g_env.phi[i].t_last);
         pthread_create(&g_env.phi[i].thread, NULL, routine, &g_env.phi[i]);
         i += 2;
     }
